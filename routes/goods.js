ec782572d1e782d93372a6e9305797d469cbc7cc
myjade <myjade999@gmail.com>
2020-02-06 18:59:21 +0900

    edit category js 구현

--- routes/goods.js
+++ routes/goods.js
@@ -2,6 +2,7 @@ var path = require('path');
 var express = require('express');
 var router = express.Router();
 
+
 router.get('/', function(req, res, next) {
     res.render('goods/index', {
         path: '/goods/',
